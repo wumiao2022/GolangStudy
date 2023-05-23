67abd181e7cce0fa3d065e3dcf647dc30e0348f9
root <root@vultr.guest>
2023-05-23 19:11:23 +0000

    Study on 05/23

---
+++ 2023-05-23-1911.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	// 1. 定义一个字符串变量
+	str := "Hello, world!"
+
+	// 2. 打印字符串变量
+	fmt.Println(str)
+
+	// 3. 定义一个整型变量
+	num := 123
+
+	// 4. 打印整型变量
+	fmt.Println(num)
+
+	// 5. 定义一个浮点型变量
+	flo := 3.14
+
+	// 6. 打印浮点型变量
+	fmt.Println(flo)
+
+	// 7. 定义一个布尔型变量
+	boo := true
+
+	// 8. 打印布尔型变量
+	fmt.Println(boo)
+
+	// 9. 定义一个空值变量
+	var emp interface{}
+
+	// 10. 打印空值变量
+	fmt.Println(emp)
+}
