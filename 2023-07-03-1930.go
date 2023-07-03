37fe326321c8abeffb8d7e1465e89076feeda0b3
root <root@vultr.guest>
2023-07-03 19:30:37 +0000

    Study on 07/03

---
+++ 2023-07-03-1930.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+func main() {
+	// 练习1：打印出 "Hello, world!"
+	fmt.Println("Hello, world!")
+
+	// 练习2：定义一个整型变量x，并给它赋值为10
+	x := 10
+	fmt.Println(x)
+
+	// 练习3：定义一个字符串变量name，并给它赋值为"John"
+	name := "John"
+	fmt.Println(name)
+
+	// 练习4：定义一个布尔型变量isTrue，并给它赋值为true
+	isTrue := true
+	fmt.Println(isTrue)
+
+	// 练习5：定义一个浮点型变量pi，并给它赋值为3.14
+	pi := 3.14
+	fmt.Println(pi)
+}
