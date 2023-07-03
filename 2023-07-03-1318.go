506173b577edde82c2c4532b98012b19900d934c
root <root@vultr.guest>
2023-07-03 13:18:33 +0000

    Study on 07/03

---
+++ 2023-07-03-1318.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func main() {
+    // 1. 声明一个整数变量num，并将其初始化为10
+    num := 10
+
+    // 2. 声明一个字符串变量name，并将其初始化为"GoLang"
+    name := "GoLang"
+
+    // 3. 声明一个布尔类型变量status，并将其初始化为true
+    status := true
+
+    // 4. 打印num的值
+    fmt.Println(num)
+
+    // 5. 打印name的值
+    fmt.Println(name)
+
+    // 6. 打印status的值
+    fmt.Println(status)
+}
