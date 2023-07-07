8bd5422404f7920b59d67f23690b4e7f77ae8723
root <root@vultr.guest>
2023-07-07 18:25:16 +0000

    Study on 07/07

---
+++ 2023-07-07-1825.go
@@ -0,0 +1,35 @@
+package main
+
+import "fmt"
+
+func main() {
+	// 1. 声明一个整数变量x，并赋值为10
+	x := 10
+
+	// 2. 打印变量x的值
+	fmt.Println(x)
+
+	// 3. 声明一个字符串变量name，并赋值为"GoLang"
+	name := "GoLang"
+
+	// 4. 打印变量name的值
+	fmt.Println(name)
+
+	// 5. 声明一个浮点数变量pi，并赋值为3.14159
+	pi := 3.14159
+
+	// 6. 打印变量pi的值
+	fmt.Println(pi)
+
+	// 7. 声明一个布尔型变量isGoLang，并赋值为true
+	isGoLang := true
+
+	// 8. 打印变量isGoLang的值
+	fmt.Println(isGoLang)
+
+	// 9. 声明一个切片变量numbers，并初始化为包含0到9的所有整数
+	numbers := []int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9}
+
+	// 10. 打印变量numbers的值
+	fmt.Println(numbers)
+}
