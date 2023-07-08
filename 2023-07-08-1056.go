707de28cdf54816e57e8a75b744ab6d21f728122
root <root@vultr.guest>
2023-07-08 10:56:32 +0000

    Study on 07/08

---
+++ 2023-07-08-1056.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+func main() {
+	// 练习1：声明一个整型变量x，并赋予初始值为10，打印出x的值
+	x := 10
+	fmt.Println(x)
+	
+	// 练习2：声明一个字符串变量name，并赋予初始值为"John"，打印出name的值
+	name := "John"
+	fmt.Println(name)
+	
+	// 练习3：声明一个浮点型变量pi，并赋予初始值为3.1415，打印出pi的值
+	pi := 3.1415
+	fmt.Println(pi)
+	
+	// 练习4：声明一个布尔型变量isTrue，并赋予初始值为true，打印出isTrue的值
+	isTrue := true
+	fmt.Println(isTrue)
+	
+	// 练习5：声明一个整型数组numbers，包含数值1到5，打印出数组的所有元素
+	numbers := [5]int{1, 2, 3, 4, 5}
+	fmt.Println(numbers)
+}
