1e40f1272bf214d18531bec2cf6b2430d5555a6f
root <root@vultr.guest>
2023-07-08 15:29:53 +0000

    Study on 07/08

---
+++ 2023-07-08-1529.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+    // 练习1：打印Hello, World!
+    fmt.Println("Hello, World!")
+
+    // 练习2：定义一个整型变量num，赋值为10，并打印输出
+    num := 10
+    fmt.Println(num)
+
+    // 练习3：定义一个字符串变量str，赋值为"Hello, Golang!"，并打印输出
+    str := "Hello, Golang!"
+    fmt.Println(str)
+
+    // 练习4：定义一个切片变量slice，包含元素1, 2, 3，并打印输出
+    slice := []int{1, 2, 3}
+    fmt.Println(slice)
+}
