07edb0acda7af35210181b1a67651da463dad3f0
root <root@vultr.guest>
2023-07-08 10:26:44 +0000

    Study on 07/08

---
+++ 2023-07-08-1026.go
@@ -0,0 +1,32 @@
+package main
+
+import "fmt"
+
+func main() {
+	// Exercise 1: Print "Hello, World!" to the console.
+	fmt.Println("Hello, World!")
+
+	// Exercise 2: Create a variable "x" with a value of 5 and print its value.
+	x := 5
+	fmt.Println(x)
+
+	// Exercise 3: Create a variable "y" with a value of 7 and print its value.
+	y := 7
+	fmt.Println(y)
+
+	// Exercise 4: Add "x" and "y" together and print the result.
+	result := x + y
+	fmt.Println(result)
+
+	// Exercise 5: Subtract "y" from "x" and print the result.
+	result = x - y
+	fmt.Println(result)
+
+	// Exercise 6: Multiply "x" and "y" together and print the result.
+	result = x * y
+	fmt.Println(result)
+
+	// Exercise 7: Divide "x" by "y" and print the result.
+	result = x / y
+	fmt.Println(result)
+}
