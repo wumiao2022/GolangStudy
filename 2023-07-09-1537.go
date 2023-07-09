65af96d1660463d4f3925739d48e79d68527e631
root <root@vultr.guest>
2023-07-09 15:37:27 +0000

    Study on 07/09

---
+++ 2023-07-09-1537.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+func main() {
+	// 练习1：打印Hello, World!
+	fmt.Println("Hello, World!")
+
+	// 练习2：声明一个整数变量x并赋值为10，打印出x的值
+	var x int = 10
+	fmt.Println(x)
+
+	// 练习3：声明一个字符串变量name并赋值为"GoLang"，打印出name的值
+	var name string = "GoLang"
+	fmt.Println(name)
+
+	// 练习4：声明一个布尔变量isTrue并赋值为true，打印出isTrue的值
+	var isTrue bool = true
+	fmt.Println(isTrue)
+
+	// 练习5：声明一个浮点数变量num并赋值为3.14，打印出num的值
+	var num float64 = 3.14
+	fmt.Println(num)
+}
