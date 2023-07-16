4a71562f86e6c8f25db14a4853884f5ac08f18f1
root <root@vultr.guest>
2023-07-16 18:02:40 +0000

    Study on 07/16

---
+++ 2023-07-16-1802.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	// 练习1：打印Hello, World!
+	fmt.Println("Hello, World!")
+
+	// 练习2：计算并打印2的10次方
+	fmt.Println(2 << 9)
+
+	// 练习3：判断一个数是否是奇数
+	num := 7
+	if num%2 == 0 {
+		fmt.Println(num, "是偶数")
+	} else {
+		fmt.Println(num, "是奇数")
+	}
+
+	// 练习4：将一个字符串反转
+	str := "Hello, World!"
+	reversedStr := ""
+	for i := len(str) - 1; i >= 0; i-- {
+		reversedStr += string(str[i])
+	}
+	fmt.Println(reversedStr)
+}
