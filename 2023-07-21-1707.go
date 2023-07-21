07abf51f23c9700a97d7f401986261c1f6929bcc
root <root@vultr.guest>
2023-07-21 17:07:59 +0000

    Study on 07/21

---
+++ 2023-07-21-1707.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+func main() {
+	// 练习1：打印Hello, World!
+	fmt.Println("Hello, World!")
+
+	// 练习2：计算并打印1+2的结果
+	result := 1 + 2
+	fmt.Println(result)
+
+	// 练习3：将字符串"Go"和"lang"拼接起来并打印
+	str1 := "Go"
+	str2 := "lang"
+	fmt.Println(str1 + str2)
+
+	// 练习4：声明一个整型变量并赋值为10，打印该变量的值
+	num := 10
+	fmt.Println(num)
+
+	// 练习5：声明一个浮点型变量并赋值为3.14，打印该变量的值
+	floatNum := 3.14
+	fmt.Println(floatNum)
+}
