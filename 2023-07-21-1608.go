2a2d9ece61b7129b45411e731c14ebb0984af5a0
root <root@vultr.guest>
2023-07-21 16:08:42 +0000

    Study on 07/21

---
+++ 2023-07-21-1608.go
@@ -0,0 +1,35 @@
+package main
+
+import "fmt"
+
+func main() {
+	// 0. 声明一个整型变量x，并赋值为10
+	x := 10
+
+	// 1. 声明一个字符串变量name，并赋值为"Go"
+	name := "Go"
+
+	// 2. 声明一个布尔变量isTrue，并赋值为true
+	isTrue := true
+
+	// 3. 声明一个浮点数变量pi，并赋值为3.14159
+	pi := 3.14159
+
+	// 4. 声明一个整型数组arr，包含5个元素，数值为1, 2, 3, 4, 5
+	arr := [5]int{1, 2, 3, 4, 5}
+
+	// 5. 打印变量x的值
+	fmt.Println(x)
+
+	// 6. 打印变量name的值
+	fmt.Println(name)
+
+	// 7. 打印变量isTrue的值
+	fmt.Println(isTrue)
+
+	// 8. 打印变量pi的值
+	fmt.Println(pi)
+
+	// 9. 打印数组arr的值
+	fmt.Println(arr)
+}
