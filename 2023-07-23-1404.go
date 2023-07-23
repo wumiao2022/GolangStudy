1d6ecb19db8003e61f1f485dfa70e6ff26ad678d
root <root@vultr.guest>
2023-07-23 14:04:22 +0000

    Study on 07/23

---
+++ 2023-07-23-1404.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+func main() {
+	// Print "Hello, World!" to the console
+	fmt.Println("Hello, World!")
+
+	// Declare a variable named "age" with the value of 25
+	age := 25
+
+	// Print the value of "age" to the console
+	fmt.Println("Age:", age)
+
+	// Create an array of integers with the values 1, 2, 3, 4, 5
+	numbers := [5]int{1, 2, 3, 4, 5}
+
+	// Print the length of the array
+	fmt.Println("Number of elements in array:", len(numbers))
+
+	// Declare a variable named "name" with the value "John Doe"
+	name := "John Doe"
+
+	// Print the value of "name" to the console
+	fmt.Println("Name:", name)
+}
