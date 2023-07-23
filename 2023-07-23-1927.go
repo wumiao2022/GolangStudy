5c67b10d54ffa35f3a03e24046c578c562a64053
root <root@vultr.guest>
2023-07-23 19:27:11 +0000

    Study on 07/23

---
+++ 2023-07-23-1927.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func main() {
+    // 练习1：打印Hello, World!
+    fmt.Println("Hello, World!")
+
+    // 练习2: 计算1+2的结果并打印
+    fmt.Println(1 + 2)
+
+    // 练习3: 判断10是否大于5并打印结果
+    fmt.Println(10 > 5)
+
+    // 练习4: 定义一个整数变量x并给其赋值为5，打印其值
+    x := 5
+    fmt.Println(x)
+
+    // 练习5: 定义一个字符串变量name并给其赋值为"Go语言"，打印其值
+    name := "Go语言"
+    fmt.Println(name)
+}
