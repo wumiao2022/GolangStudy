099ffee9cc05fea8dc66e91b2f626aee56371812
root <root@vultr.guest>
2023-08-13 19:42:05 +0000

    Study on 08/13

---
+++ 2023-08-13-1942.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+func main() {
+	rand.Seed(time.Now().UnixNano())
+
+	for i := 1; i <= 10; i++ {
+		num := rand.Intn(100)
+		fmt.Printf("随机数%d: %d\n", i, num)
+	}
+}
