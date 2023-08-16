beb1488aa3b883a2bca877b356bffcd181766e06
root <root@vultr.guest>
2023-08-16 18:06:50 +0000

    Study on 08/16

---
+++ 2023-08-16-1806.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	// 1. 请打印出 Hello, World!
+
+	// 2. 请定义一个名为 message 的变量，使用 "Hello, Golang!" 进行初始化
+
+	// 3. 请定义一个名为 num1 的变量，并将其赋值为 10
+
+	// 4. 请定义一个名为 num2 的变量，并将其赋值为 5.5
+
+	// 5. 请定义一个名为 result 的变量，并将 num1 与 num2 相加后的结果赋值给 result
+
+	// 6. 请定义一个名为 age 的变量，并将其赋值为 25。然后打印出 "I am 25 years old."
+
+	// 7. 请定义一个名为 username 的变量，并将其赋值为你的名字。然后打印出 "My name is {你的名字}."
+
+	// 8. 请定义一个名为 isTrue 的布尔类型变量，并将其赋值为 true
+
+	// 9. 请定义一个名为 score 的变量，并将其赋值为 85。然后根据 score 值的不同，打印出 "Pass" 或者 "Fail"
+
+	// 10. 请定义一个名为 weight 的变量，并将其赋值为 70。然后打印出 "You are overweight." 如果 weight 大于 50，否则打印出 "You are not overweight."
+}
