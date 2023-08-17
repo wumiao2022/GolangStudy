d92738526694332b7b495d1814662d96754753f5
root <root@vultr.guest>
2023-08-17 17:06:06 +0000

    Study on 08/17

---
+++ 2023-08-17-1706.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+func main() {
+	// 1. 声明一个整数变量a，并将其值设置为10
+	var a int = 10
+	fmt.Println(a)
+
+	// 2. 声明两个整数变量b和c，并将其值分别设置为20和30
+	var b, c int = 20, 30
+	fmt.Println(b, c)
+
+	// 3. 声明一个浮点数变量d，并将其值设置为3.14
+	var d float64 = 3.14
+	fmt.Println(d)
+
+	// 4. 声明一个字符串变量e，并将其值设置为"Hello, World!"
+	var e string = "Hello, World!"
+	fmt.Println(e)
+
+	// 5. 声明一个布尔变量f，并将其值设置为true
+	var f bool = true
+	fmt.Println(f)
+}
