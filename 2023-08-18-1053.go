bf4d46f7acc18d9d204ba745acc1d2580b851829
root <root@vultr.guest>
2023-08-18 10:53:48 +0000

    Study on 08/18

---
+++ 2023-08-18-1053.go
@@ -0,0 +1,38 @@
+package main
+
+import "fmt"
+
+func main() {
+	// Exercise 1: Print "Hello, world!" to the console
+	fmt.Println("Hello, world!")
+
+	// Exercise 2: Declare a variable and assign it a string value
+	message := "This is a string"
+
+	// Exercise 3: Print the value of the variable
+	fmt.Println(message)
+
+	// Exercise 4: Declare an integer variable and assign it a value
+	num := 10
+
+	// Exercise 5: Print the value of the integer variable
+	fmt.Println(num)
+
+	// Exercise 6: Declare a float variable and assign it a value
+	floatNum := 3.14
+
+	// Exercise 7: Print the value of the float variable
+	fmt.Println(floatNum)
+
+	// Exercise 8: Perform an arithmetic operation using the integer and float variables
+	result := floatNum * float64(num)
+
+	// Exercise 9: Print the result of the arithmetic operation
+	fmt.Println(result)
+
+	// Exercise 10: Declare a boolean variable and assign it a value
+	isTrue := true
+
+	// Exercise 11: Print the value of the boolean variable
+	fmt.Println(isTrue)
+}
