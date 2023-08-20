3e29edf2e26a4602db8123632bd17029e6084eee
root <root@vultr.guest>
2023-08-20 13:24:51 +0000

    Study on 08/20

---
+++ 2023-08-20-1324.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+func main() {
+	// 练习一: 打印Hello, World!
+	fmt.Println("Hello, World!")
+
+	// 练习二: 创建一个整数变量x，并将其赋值为5，然后打印出x的值
+	var x int = 5
+	fmt.Println(x)
+
+	// 练习三: 创建一个字符串变量name，并将其赋值为你的名字，然后打印出name的值
+	var name string = "John"
+	fmt.Println(name)
+
+	// 练习四: 创建一个布尔变量isTrue，并将其赋值为true，然后打印出isTrue的值
+	var isTrue bool = true
+	fmt.Println(isTrue)
+
+	// 练习五: 创建一个浮点数变量pi，并将其赋值为3.14159，然后打印出pi的值
+	var pi float64 = 3.14159
+	fmt.Println(pi)
+}
+
+上述代码包含了五个练习实例，分别打印出"Hello, World!"、整数、字符串、布尔和浮点数的值。
