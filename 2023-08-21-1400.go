76bdf008a2c21419d673f4147ebf9a85eab9fda4
root <root@vultr.guest>
2023-08-21 14:00:27 +0000

    Study on 08/21

--- 2023-08-21-1400.go
+++ 2023-08-21-1400.go
@@ -6,40 +6,23 @@ func main() {
 	// 练习1：打印Hello, World!
 	fmt.Println("Hello, World!")
 
-	// 练习2：求两个整数的和
-	num1 := 10
-	num2 := 20
-	sum := num1 + num2
-	fmt.Printf("Sum: %d\n", sum)
-
-	// 练习3：判断一个数是否为偶数
-	number := 7
-	if number%2 == 0 {
-		fmt.Printf("%d is even\n", number)
-	} else {
-		fmt.Printf("%d is odd\n", number)
+	// 练习2：打印数字1到10
+	for i := 1; i <= 10; i++ {
+		fmt.Println(i)
 	}
 
-	// 练习4：将一个字符串反转并输出
-	str := "Hello, Golang"
-	reversedStr := ""
-	for i := len(str) - 1; i >= 0; i-- {
-		reversedStr += string(str[i])
+	// 练习3：计算1到100的和
+	sum := 0
+	for i := 1; i <= 100; i++ {
+		sum += i
 	}
-	fmt.Println(reversedStr)
+	fmt.Println("和为：", sum)
 
-	// 练习5：打印1到100之间的所有素数
-	for num := 2; num <= 100; num++ {
-		isPrime := true
-		for i := 2; i <= num/2; i++ {
-			if num%i == 0 {
-				isPrime = false
-				break
-			}
+	// 练习4：打印九九乘法表
+	for i := 1; i <= 9; i++ {
+		for j := 1; j <= i; j++ {
+			fmt.Printf("%d * %d = %d\t", j, i, i*j)
 		}
-		if isPrime {
-			fmt.Printf("%d ", num)
-		}
+		fmt.Println()
 	}
-	fmt.Println()
 }
