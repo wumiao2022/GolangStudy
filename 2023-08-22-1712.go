fec6738e4929eca9154f69c2feb6485160508951
root <root@vultr.guest>
2023-08-22 17:12:16 +0000

    Study on 08/22

---
+++ 2023-08-22-1712.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+func main() {
+    // 练习1：打印Hello, World!
+    fmt.Println("Hello, World!")
+
+    // 练习2：定义一个整数变量并赋值为10，打印该变量的值
+    var num int = 10
+    fmt.Println(num)
+
+    // 练习3：定义一个字符串变量并赋值为"Hello, Golang!"，打印该变量的值
+    var str string = "Hello, Golang!"
+    fmt.Println(str)
+
+    // 练习4：定义一个浮点数变量并赋值为3.14，打印该变量的值
+    var floatNum float64 = 3.14
+    fmt.Println(floatNum)
+
+    // 练习5：定义一个布尔变量并赋值为true，打印该变量的值
+    var boolVal bool = true
+    fmt.Println(boolVal)
+}
