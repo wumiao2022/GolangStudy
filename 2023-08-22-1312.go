caa65c903432079b1fe17dc72f605544e63d53b1
root <root@vultr.guest>
2023-08-22 13:12:21 +0000

    Study on 08/22

---
+++ 2023-08-22-1312.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+func main() {
+	// 1. 打印 "Hello, World!"
+
+	// 2. 声明一个变量a，并将其赋值为20
+
+	// 3. 声明一个切片b，包含元素1, 2, 3, 4, 5
+
+	// 4. 声明一个映射c，包含键值对"apple": "red", "banana": "yellow", "grape": "purple"
+
+	// 5. 使用循环语句for打印出切片b的所有元素
+
+	// 6. 使用条件语句if判断变量a是否大于10，如果是，则打印 "a is greater than 10"，否则打印 "a is less than or equal to 10"
+
+	// 7. 创建一个名为add的函数，接受两个整数参数，并返回它们的和
+
+	// 8. 调用add函数并打印结果
+
+	// 9. 创建一个匿名函数，将其赋值给变量multiply，该函数接受两个整数参数，并返回它们的乘积
+
+	// 10. 调用multiply函数并打印结果
+}
