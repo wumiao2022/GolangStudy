ab557624cc2baade7610e9400aa54ecbc5d42aee
walter.wu <wumiao2022@gmail.com>
2023-08-28 11:49:10 +0000

    Study on 08/28

---
+++ 2023-08-28-1149.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+func main() {
+	// 练习1：输出Hello World
+	fmt.Println("Hello World")
+
+	// 练习2：声明一个整数变量，赋值为10，打印输出
+	var num int = 10
+	fmt.Println(num)
+
+	// 练习3：声明一个字符串变量，赋值为"Go语言好好玩"，打印输出
+	var str string = "Go语言好好玩"
+	fmt.Println(str)
+
+	// 练习4：声明一个布尔变量，赋值为true，打印输出
+	var flag bool = true
+	fmt.Println(flag)
+
+	// 练习5：声明一个浮点型变量，赋值为3.14，打印输出
+	var pi float64 = 3.14
+	fmt.Println(pi)
+}
