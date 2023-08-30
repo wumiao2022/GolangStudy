0714c0a063ec4db78a7f4fc2f6499495192f7fe0
walter.wu <wumiao2022@gmail.com>
2023-08-30 16:28:50 +0000

    Study on 08/30

---
+++ 2023-08-30-1628.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+    "fmt"
+)
+
+func main() {
+    // 1. 声明一个整型变量 x，并赋值为 5
+    x := 5
+
+    // 2. 打印变量 x 的值
+    fmt.Println(x)
+
+    // 3. 将变量 x 的值加上 10，并将结果重新赋值给变量 x
+    x = x + 10
+
+    // 4. 打印变量 x 的值
+    fmt.Println(x)
+
+    // 5. 声明一个字符串变量 name，并赋值为 "Golang"
+    name := "Golang"
+
+    // 6. 打印变量 name 的值
+    fmt.Println(name)
+
+    // 7. 将变量 name 的值修改为 "Go language"
+    name = "Go language"
+
+    // 8. 打印变量 name 的值
+    fmt.Println(name)
+}
