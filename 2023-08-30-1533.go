c5e2cfc7efb363e453a113345d619c92c3a351ac
walter.wu <wumiao2022@gmail.com>
2023-08-30 15:33:05 +0000

    Study on 08/30

---
+++ 2023-08-30-1533.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+func main() {
+	// 1. 打印Hello, World!
+	fmt.Println("Hello, World!")
+
+	// 2. 声明一个整数变量x并赋值为10，打印x的值
+	x := 10
+	fmt.Println(x)
+
+	// 3. 声明一个字符串变量name并赋值为"John"，打印name的值
+	name := "John"
+	fmt.Println(name)
+
+	// 4. 声明一个浮点数变量y并赋值为3.14，打印y的值
+	y := 3.14
+	fmt.Println(y)
+
+	// 5. 声明一个布尔型变量isTrue并赋值为true，打印isTrue的值
+	isTrue := true
+	fmt.Println(isTrue)
+}
