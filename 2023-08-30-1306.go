a27bf876379bc5935d7f0d22dde78803f5de70fd
walter.wu <wumiao2022@gmail.com>
2023-08-30 13:06:36 +0000

    Study on 08/30

---
+++ 2023-08-30-1306.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	fmt.Println("Good morning! Today's practice:")
+	fmt.Println()
+
+	// Exercise 1: Hello, World!
+	fmt.Println("Exercise 1: Hello, World!")
+	fmt.Println("func main() {")
+	fmt.Println("\tfmt.Println(\"Hello, World!\")")
+	fmt.Println("}")
+	fmt.Println()
+
+	// Exercise 2: Sum of two numbers
+	fmt.Println("Exercise 2: Sum of two numbers")
+	fmt.Println("func main() {")
+	fmt.Println("\tvar a = 10")
+	fmt.Println("\tvar b = 20")
+	fmt.Println("\tsum := a + b")
+	fmt.Println("\tfmt.Println(\"Sum of\", a, \"and\", b, \"is\", sum)")
+	fmt.Println("}")
+	fmt.Println()
+
+	// Exercise 3: Print current time
+	fmt.Println("Exercise 3: Print current time")
+	fmt.Println("func main() {")
+	fmt.Println("\tcurrentTime := time.Now()")
+	fmt.Println("\tfmt.Println(\"Current time is:\", currentTime)")
+	fmt.Println("}")
+	fmt.Println()
+
+	// Exercise 4: Find the area of a rectangle
+	fmt.Println("Exercise 4: Find the area of a rectangle")
+	fmt.Println("func main() {")
+	fmt.Println("\tvar length = 5.4")
+	fmt.Println("\tvar width = 3.2")
+	fmt.Println("\tarea := length * width")
+	fmt.Println("\tfmt.Println(\"Area of the rectangle is\", area)")
+	fmt.Println("}")
+	fmt.Println()
+
+	fmt.Println("Have a great day of coding!")
+}
+
+This is today's practice! Happy coding!
