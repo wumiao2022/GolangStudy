ee2cec0059ac48da411e8df96c673d2e01759b92
walter.wu <wumiao2022@gmail.com>
2023-09-07 10:28:04 +0000

    Study on 09/07

---
+++ 2023-09-07-1028.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func main() {
+	// 练习1：打印Hello World
+	fmt.Println("Hello World")
+
+	// 练习2：计算并输出1+1的结果
+	fmt.Println(1 + 1)
+
+	// 练习3：声明一个整数变量x，并赋值为10，然后输出x的值
+	var x int = 10
+	fmt.Println(x)
+
+	// 练习4：声明一个字符串变量name，并赋值为"GoLang"，然后输出name的值
+	var name string = "GoLang"
+	fmt.Println(name)
+
+	// 练习5：声明一个布尔型变量isTrue，并赋值为true，然后输出isTrue的值
+	var isTrue bool = true
+	fmt.Println(isTrue)
+}
