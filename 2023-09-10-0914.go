f5ee1a1e532d4d6614c65c071eead67db6418e56
walter.wu <wumiao2022@gmail.com>
2023-09-10 09:14:05 +0000

    Study on 09/10

---
+++ 2023-09-10-0914.go
@@ -0,0 +1,37 @@
+package main
+
+import "fmt"
+
+func main() {
+    // 1. 交换两个变量的值
+    a := 1
+    b := 2
+    
+    // 请在此处编写代码
+    
+    fmt.Println(a, b) // 输出为 "2 1"
+    
+    // 2. 求n的阶乘
+    n := 5
+    
+    // 请在此处编写代码
+    
+    fmt.Println(result) // 输出为 "120"
+    
+    // 3. 将一个字符串反转
+    str := "Hello, Go!"
+    
+    // 请在此处编写代码
+    
+    fmt.Println(reversedStr) // 输出为 "!oG ,olleH"
+    
+    // 4. 判断一个数是否是素数
+    num := 17
+    
+    // 请在此处编写代码
+    
+    fmt.Println(isPrime) // 输出为 "true"
+    
+    // 5. 打印九九乘法表
+    // 请在此处编写代码
+}
