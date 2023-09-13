ec38e10e9db14976cb757d6a02bf61c2d0d9f58b
walter.wu <wumiao2022@gmail.com>
2023-09-13 16:48:06 +0000

    Study on 09/13

---
+++ 2023-09-13-1648.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+func main() {
+	// 练习1：输出Hello, World!
+	fmt.Println("Hello, World!")
+
+	// 练习2：输出整数相加的结果
+	num1 := 10
+	num2 := 20
+	fmt.Println(num1 + num2)
+
+	// 练习3：输出字符串的长度
+	str := "Golang"
+	fmt.Println(len(str))
+
+	// 练习4：判断两个整数是否相等
+	num3 := 30
+	num4 := 30
+	fmt.Println(num3 == num4)
+
+	// 练习5：输出一个整数的平方
+	num5 := 5
+	fmt.Println(num5 * num5)
+}
