3cbcb77e119d9954b68a06146b9e48bc5e672174
walter.wu <wumiao2022@gmail.com>
2023-09-17 16:36:10 +0000

    Study on 09/17

---
+++ 2023-09-17-1636.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+func main() {
+	// 练习1：打印Hello, World!
+	fmt.Println("Hello, World!")
+
+	// 练习2：声明一个整数变量并赋值为10，打印出变量的值
+	num := 10
+	fmt.Println(num)
+
+	// 练习3：声明一个字符串变量并赋值为"Go语言"
+	str := "Go语言"
+	fmt.Println(str)
+
+	// 练习4：声明一个布尔变量并赋值为true，打印出变量的值
+	isTrue := true
+	fmt.Println(isTrue)
+
+	// 练习5：声明一个整数数组并初始化为1, 2, 3, 4, 5，打印出数组的值
+	arr := []int{1, 2, 3, 4, 5}
+	fmt.Println(arr)
+}
