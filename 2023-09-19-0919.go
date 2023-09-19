c82ad1db412420c4708fff68ca20cf2595f8d464
walter.wu <wumiao2022@gmail.com>
2023-09-19 09:19:41 +0000

    Study on 09/19

---
+++ 2023-09-19-0919.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	// Question 1: Declare a string variable "name" and assign your name as the value
+
+	// Question 2: Declare an integer variable "age" and assign your age as the value
+
+	// Question 3: Declare a boolean variable "isTrue" and assign either true or false as the value
+
+	// Question 4: Declare a float64 variable "temperature" and assign a decimal number as the value
+
+	// Question 5: Print out the values of all the variables
+
+	// Question 6: Create a slice "numbers" with the values 1, 2, 3, 4, 5
+
+	// Question 7: Print out the length of the slice "numbers"
+
+	// Question 8: Append the value 6 to the slice "numbers"
+
+	// Question 9: Print out the new length of the slice "numbers"
+
+	// Question 10: Create a for loop that iterates through the slice "numbers" and prints out each value
+}
