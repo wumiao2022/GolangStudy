85d509215f732b06b16b46b89c6e519e80e557a9
walter.wu <wumiao2022@gmail.com>
2023-09-20 15:19:25 +0000

    Study on 09/20

---
+++ 2023-09-20-1519.go
@@ -0,0 +1,46 @@
+package main
+
+import "fmt"
+
+func main() {
+	// 1. 打印 "Hello, World!"
+
+	// 2. 定义一个名为name的变量并赋值为你的名字，然后打印出来
+	
+	// 3. 定义一个整数数组nums，包含10个数字，并打印出该数组
+	
+	// 4. 计算并打印出1到100之间所有偶数的和
+	
+	// 5. 创建一个函数，接受一个字符串和一个整数，然后打印出该字符串连续重复n次的结果
+}
+
+// 示例答案：
+
+// 1.
+// fmt.Println("Hello, World!")
+
+// 2.
+// name := "John"
+// fmt.Println(name)
+
+// 3.
+// nums := [10]int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+// fmt.Println(nums)
+
+// 4.
+// sum := 0
+// for i := 1; i <= 100; i++ {
+//     if i % 2 == 0 {
+//         sum += i
+//     }
+// }
+// fmt.Println(sum)
+
+// 5.
+// func repeatString(str string, n int) {
+//     for i := 0; i < n; i++ {
+//         fmt.Println(str)
+//     }
+// }
+
+// repeatString("Hello", 3)
