b799c351970106ad92c30242e85d502796c134a6
walter.wu <wumiao2022@gmail.com>
2023-09-23 09:30:42 +0000

    Study on 09/23

---
+++ 2023-09-23-0930.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+	// 练习1：输出Hello World!
+	fmt.Println("Hello World!")
+
+	// 练习2：定义一个整型变量x，并将其初始化为10，然后将x的值输出
+	x := 10
+	fmt.Println(x)
+
+	// 练习3：定义一个字符串变量name，并将其初始化为"GoLang"，然后将name的值输出
+	name := "GoLang"
+	fmt.Println(name)
+
+	// 练习4：定义一个布尔型变量isTrue，并将其初始化为true，然后将isTrue的值输出
+	isTrue := true
+	fmt.Println(isTrue)
+}
