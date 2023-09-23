e0f856950ab7176c3e66c7c360f18778d45927be
walter.wu <wumiao2022@gmail.com>
2023-09-23 13:14:29 +0000

    Study on 09/23

---
+++ 2023-09-23-1314.go
@@ -0,0 +1,30 @@
+package main
+
+import "fmt"
+
+func main() {
+	// 练习1：打印Hello, World!
+	fmt.Println("Hello, World!")
+
+	// 练习2：定义一个整数变量并打印其值
+	num := 10
+	fmt.Println(num)
+
+	// 练习3：定义一个字符串变量并打印其值
+	str := "Golang is awesome!"
+	fmt.Println(str)
+
+	// 练习4：定义一个多行字符串变量并打印其值
+	multiLineStr := `This is
+	a multi-line
+	string.`
+	fmt.Println(multiLineStr)
+
+	// 练习5：定义一个布尔变量并打印其值
+	boolVar := true
+	fmt.Println(boolVar)
+
+	// 练习6：定义一个浮点数变量并打印其值
+	floatVar := 3.14
+	fmt.Println(floatVar)
+}
